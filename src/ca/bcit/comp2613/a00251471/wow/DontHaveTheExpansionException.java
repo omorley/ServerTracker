74182d60efc31e6dced6d1cb28785a52ed0708d3
omorley <Owen@10.100.68.19>
2014-06-17 23:10:48 -0700

    Adding mid-term first draft code. Added ca.bcit.comp2613.a00251471.util package Added human race Fixed rollCharacters bug Sorted characters by level, strength then armour Printing characters in a readable format Implemented horde spy removal Checking for pandaren race, throwing exception if found Added efficient method to find character missing

---
+++ src/ca/bcit/comp2613/a00251471/wow/DontHaveTheExpansionException.java
@@ -0,0 +1,8 @@
+package ca.bcit.comp2613.a00251471.wow;
+
+public class DontHaveTheExpansionException extends Exception {
+	public DontHaveTheExpansionException (String message) {
+		super(message);
+	}
+
+}
---
+++ src/ca/bcit/comp2613/a00251471/wow/Race.java
@@ -0,0 +1,6 @@
+package ca.bcit.comp2613.a00251471.wow;
+
+public enum Race {
+	WORGEN, DRAENEI, DWARF, GNOME, ORC, PANDAREN, HUMAN
+	// TODO, add a HUMAN Race
+}
